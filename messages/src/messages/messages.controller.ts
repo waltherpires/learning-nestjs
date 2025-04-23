69f6c43a52ef413c9b671b78c2cf240675f4bee5
Walther Oliveira <waltherpires@hotmail.com>
2025-04-23 01:15:30 -0300

    reporta erro com NotFoundException

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,29 +1,42 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-    messagesService: MessagesService;
+  messagesService: MessagesService;
 
-    constructor() {
-        // DON'T DO THIS ON REAL APP
-        // USE DEPEDENCY INJECTION
-        this.messagesService = new MessagesService();
+  constructor() {
+    // DON'T DO THIS ON REAL APP
+    // USE DEPEDENCY INJECTION
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
     }
 
-    @Get()
-    listMessages() {
-        return this.messagesService.findAll();
-    }
-
-    @Post()
-    createMessage(@Body() body: CreateMessageDto) {
-        return this.messagesService.create(body.content);
-    }
-
-    @Get('/:id')
-    getMessage(@Param('id') id: string) {
-        return this.messagesService.findOne(id);
-    }
+    return message;
+  }
 }
