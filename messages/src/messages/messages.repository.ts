05389160bbd9396eaab2c9a02f9f02ce153d98aa
Walther Oliveira <waltherpires@hotmail.com>
2025-04-23 03:16:22 -0300

    adiciona dependency injection

--- messages/src/messages/messages.repository.ts
+++ messages/src/messages/messages.repository.ts
@@ -1,6 +1,8 @@
 
+import { Injectable } from "@nestjs/common";
 import { readFile, writeFile } from 'fs/promises';
 
+@Injectable()
 export class MessagesRepository {
     async findOne(id: string) {
         const contents = await readFile('messages.json', 'utf8');
